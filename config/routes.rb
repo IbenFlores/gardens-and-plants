3a0704c7a81a58f6fdfa90564949912541e56c37
Iben Polanco <iben.fpolanco@gmail.com>
2022-10-15 18:30:55 -0500

    rails-front-end livecode 964

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'plant_tags/new'
   resources :gardens do
     resources :plants, only: :create
   end
+
+  resources :plants, except: :create do
+    resources :plant_tags, only: [:new, :create]
+  end
+  resources :plant_tags, only: :destroy
   resources :plants, only: :destroy
 end
---
+++ db/migrate/20221015224154_change_category_to_name.rb
@@ -0,0 +1,6 @@
+class ChangeCategoryToName < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :tags, :category, :name
+    #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
+  end
+end
